b852ba8da9657b33f00c8355d4b0ad1e85cab6b2
shiblo-dev <devshiblo6@gmail.com>
2026-08-02 14:21:42 +0600

    HowItWorks  section added

--- app/(publicGroup)/page.tsx
+++ app/(publicGroup)/page.tsx
@@ -1,6 +1,7 @@
 import Categories from "@/components/home/Categories";
 import FeaturedGear from "@/components/home/FeaturedGear";
 import Hero from "@/components/home/Hero";
+import HowItWorks from "@/components/home/HowItWorks";
 import WhyChooseUs from "@/components/home/WhyChooseUs";
 
 export default function HomePage() {
@@ -10,6 +11,8 @@ export default function HomePage() {
       <Categories />
       <FeaturedGear />
       <WhyChooseUs />
+      <HowItWorks />
+
 
     </main>
   );
