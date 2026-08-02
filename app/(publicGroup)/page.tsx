c62c288eaaa2c76d00d35999e1b0ab4c37e1fd38
shiblo-dev <devshiblo6@gmail.com>
2026-08-02 14:20:36 +0600

    why choose us  section added

--- app/(publicGroup)/page.tsx
+++ app/(publicGroup)/page.tsx
@@ -1,14 +1,6 @@
-import BecomeProvider from "@/components/home/BecomeProvider";
 import Categories from "@/components/home/Categories";
-import FAQ from "@/components/home/FAQ";
 import FeaturedGear from "@/components/home/FeaturedGear";
-import Footer from "@/components/home/Footer";
-
 import Hero from "@/components/home/Hero";
-import HowItWorks from "@/components/home/HowItWorks";
-import Newsletter from "@/components/home/Newsletter";
-import Stats from "@/components/home/Stats";
-import Testimonials from "@/components/home/Testimonials";
 import WhyChooseUs from "@/components/home/WhyChooseUs";
 
 export default function HomePage() {
@@ -17,7 +9,7 @@ export default function HomePage() {
       <Hero />
       <Categories />
       <FeaturedGear />
-        
+      <WhyChooseUs />
 
     </main>
   );
